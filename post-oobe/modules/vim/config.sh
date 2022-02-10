e68db576b8d20146b338fc045044136cef40eec0
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-02-10 11:26:57 +0800

    vim - checking if exist .vimrc at home directory

--- post-oobe/modules/vim/config.sh
+++ post-oobe/modules/vim/config.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-TEMP_CONFIG="~/vimrc"
+TARGET_CONFIG=~/.vimrc
 USE_DEFAULT=1
-
-
-# Create the target configuration file
-#touch $TEMP_CONFIG
 
 # Define function whether using default template
 function use_default (){
@@ -34,23 +30,33 @@ function Interactive (){
 	fi
 
 	if [[ "$choice" == "Y" || "$choice" == "y" ]] ; then
-		#echo $CONFIG >> $TEMP_CONFIG
-		echo -e "Configuring \033[32m '$CONFIG' \033[0m"
+		echo -e "Configuring \033[32m $CONFIG \033[0m"
 		# TODO print the config first, will write to file later
 		echo $CONFIG
 	else
-		echo "Ignore settings of '$CONFIG'"
+		echo "Ignore settings of $CONFIG"
 	fi
 }
 
+# Step 1: select using default or config manually
 # confirm if igonre set by using default
 use_default
-echo $USE_DEFAULT
 
-if [ "$USE_EDFAULT" -eq 1 ]; then
-	# TODO copy file
-	echo "Copying default settings to [.vimrc]"
+if [ "$USE_DEFAULT" -eq 1 ]; then
+	echo "Copying default settings to [~/.vimrc]"
+	cp vimrc.default $TARGET_CONFIG
 else
+	# Step 2: check if .vimrc already exist at home directory, create if not exist
+	# check if .vimrc exist on user home directory
+	if [ ! -f $TARGET_CONFIG ]; then
+			echo ".vimrc file NOT found at home directory"
+			echo "Creating ~/.vimrc ..."
+			touch ~/.vimrc
+	else
+			echo ".vimrc file found at home directory"
+	fi
+
+	# Step 3: manually config by selecting each items
 	#Define items
 	# show number of line
 	PROMPT="Show numnber of line?"
