23c6a84d54588868ce3c2b4987a4b20333f402e2
Vincent Hu <vincent.guohua.hu@gmail.com>
2022-02-10 14:16:40 +0800

    vim - add more option for vim

--- post-oobe/modules/vim/config.sh
+++ post-oobe/modules/vim/config.sh
@@ -67,7 +67,69 @@ else
 	PROMPT="Highlight the line in position cursor?"
 	SETTING="set cul"
 	Interactive "$PROMPT" "$SETTING"
+
+	# show line info in status bar
+	PROMPT="show line info in status bar"
+	SETTING="set ruler"
+	Interactive "$PROMPT" "$SETTING"
+
+	# highlight the search result when do search
+	PROMPT="highlight the search result when do search"
+	SETTING="set hlsearch"
+	Interactive "$PROMPT" "$SETTING"
+
+	# when searching, jump to the match result when typing each character
+	PROMPT="when searching, jump to the match result when typing each character"
+	SETTING="set incsearch"
+	Interactive "$PROMPT" "$SETTING"
+
+	# set tab to 4 Space 
+	PROMPT="set tab to 4 Space "
+	SETTING="set ts=4"
+	Interactive "$PROMPT" "$SETTING"
+
+	# convert tab to Space
+	PROMPT="convert tab to Space"
+	SETTING="set expandtab"
+	Interactive "$PROMPT" "$SETTING"
+
+	# when press ENTER, indent for next line will keep the same as previous line
+	PROMPT="when press ENTER, indent for next line will keep the same as previous line"
+	SETTING="set autoindent"
+	Interactive "$PROMPT" "$SETTING"
+
+	# smart indent, e.g. C lauguage will auto indent when jump to next line
+	PROMPT="smart indent, e.g. C lauguage will auto indent when jump to next line"
+	SETTING="set smartindent "
+	Interactive "$PROMPT" "$SETTING"
+
+	# enable file type detect and enable specific indent 
+	PROMPT="enable file type detect and enable specific indent "
+	SETTING="filetype indent on"
+	Interactive "$PROMPT" "$SETTING"
+
+	# whenn input an left brackets, auto match right brackets
+	PROMPT="whenn input an left brackets, auto match right brackets"
+	SETTING="set showmatch"
+	Interactive "$PROMPT" "$SETTING"
+
+	# highlight syntax
+	PROMPT="highlight syntax"
+	SETTING="syntax on"
+	Interactive "$PROMPT" "$SETTING"
+
+	# when press TAB, vim wil show number of SPACE
+	PROMPT="when press TAB, vim wil show number of SPACE"
+	SETTING="set tabstop=2"
+	Interactive "$PROMPT" "$SETTING"
+
+	# press >> will increase indent, << wil decrease indent, == will cancel all indent
+	PROMPT="press >> will increase indent, << wil decrease indent, == will cancel all indent"
+	SETTING="set shiftwidth=4"
+	Interactive "$PROMPT" "$SETTING"
+
+	# enable color 256
+	PROMPT="enable color 256"
+	SETTING="set t_Co=256"
+	Interactive "$PROMPT" "$SETTING"
 fi
-
-
-
